db1d5fd59009026f607eb54cc26130d0e9559231
Vinc <vinc_944@hotmail.com>
2020-03-23 14:42:30 +0000

    display data from DB

--- config.php
+++ config.php
@@ -2,3 +2,11 @@
 
 $db = new PDO('mysql:host=DB;dbname=shapeDB','root','password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+
+$sql = "SELECT ID, ShapeName, NumberOfCorners, AreaFormula, Comments FROM shapes";
+$result = $db->query($sql);
+
+foreach ($result as $row)
+    {
+        echo "id: " . $row["ID"]. " - Shape Name: " . $row["ShapeName"]. " " . $row["NumberOfCorners"]. " " . $row["AreaFormula"] . " " . $row["Comments"]."<br>";
+    };
