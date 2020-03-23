bf9fe04f6beea8d62078c47bd8259e2a2c49271f
Vinc <vinc_944@hotmail.com>
2020-03-23 14:58:42 +0000

    changes as suggested to formatting

--- config.php
+++ config.php
@@ -3,8 +3,9 @@
 $db = new PDO('mysql:host=DB;dbname=shapeDB','root','password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
 
-$sql = "SELECT ID, ShapeName, NumberOfCorners, AreaFormula, Comments FROM shapes";
-$result = $db->query($sql);
+$query = $db->prepare("SELECT `ID`, `ShapeName`, `NumberOfCorners`, `AreaFormula`, `Comments` FROM `shapes`");
+$query->execute();
+$result = $query->fetchAll();
 
 foreach ($result as $row)
     {
