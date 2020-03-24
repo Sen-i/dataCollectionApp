73fb285a497eca232628dda1ff1b40782c64941a
Vinc <vinc_944@hotmail.com>
2020-03-24 09:11:20 +0000

    display o/p from db

--- config.php
+++ config.php
@@ -3,11 +3,4 @@
 $db = new PDO('mysql:host=DB;dbname=shapeDB','root','password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
 
-$query = $db->prepare("SELECT `ID`, `ShapeName`, `NumberOfCorners`, `AreaFormula`, `Comments` FROM `shapes`");
-$query->execute();
-$result = $query->fetchAll();
 
-foreach ($result as $row)
-    {
-        echo "id: " . $row["ID"]. " - Shape Name: " . $row["ShapeName"]. " " . $row["NumberOfCorners"]. " " . $row["AreaFormula"] . " " . $row["Comments"]."<br>";
-    };
